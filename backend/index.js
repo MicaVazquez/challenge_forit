ae3660f12909d45949073384eb252a16f75ee791
MicaVazquez <mikaela.vzqz@gmail.com>
2026-01-30 02:50:43 -0300

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let tasks = [
+  {
+    id: "1",
+    title: "aaa",
+    description: "bbb",
+    completed: false,
+    createdAt: new Date(),
+  },
+];
+
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
