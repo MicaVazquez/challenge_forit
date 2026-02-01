ccb46be46188073f9736d0e538e2616617bac467
MicaVazquez <mikaela.vzqz@gmail.com>
2026-02-01 12:06:26 -0300

    back peticiones

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
 
 const app = express();
 app.use(cors());
@@ -8,14 +9,14 @@ app.use(express.json());
 let tasks = [
   {
     id: "1",
-    title: "aaa",
+    title: "tarea1",
     description: "bbb",
     completed: false,
     createdAt: new Date(),
   },
   {
     id: "2",
-    title: "bbb",
+    title: "tarea2",
     description: "ccc",
     completed: true,
     createdAt: new Date(),
@@ -26,6 +27,57 @@ app.get("/api/tasks", (req, res) => {
   res.json(tasks);
 });
 
+app.post("/api/tasks", (req, res) => {
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    return res.status(400).json({ msj: "Faltan datos obligatorios" });
+  }
+
+  const newTask = {
+    id: crypto.randomUUID(),
+    title: title,
+    description: description,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+app.get("/api/tasks/:id", (req, res) => {
+  const task = tasks.find((t) => t.id === req.params.id);
+  if (!task) {
+    return res.status(404).json({ msj: "tarea no encontrada" });
+  }
+  res.json(task);
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+  const taskIndex = tasks.findIndex((t) => t.id === req.params.id);
+  if (taskIndex === -1) {
+    return res.status(404).json({ msj: "tarea no encontrada" });
+  }
+
+  tasks.splice(taskIndex, 1);
+  res.status(200).json({ msj: "tarea eliminada", task: req.params.id });
+});
+
+app.put("/api/tasks/:id", (req, res) => {
+  const taskIndex = tasks.findIndex((t) => t.id === req.params.id);
+
+  if (taskIndex === -1) {
+    return res.status(404).json({ msj: "tarea no encontrada" });
+  }
+  const { title, description, completed } = req.body;
+
+  if (title !== undefined) tasks[taskIndex].title = title;
+  if (description !== undefined) tasks[taskIndex].description = description;
+  if (completed !== undefined) tasks[taskIndex].completed = completed;
+  res.json(tasks[taskIndex]);
+});
+
 app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+  console.log("Servidor corriendo en http://localhost:3000 :D");
 });
